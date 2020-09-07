a9a52df28c5b29d7660db17b68b10d97459fd690
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-09-07 12:20:32 +0700

    Create jump.py

---
+++ jump.py
@@ -0,0 +1,24 @@
+import collections
+
+
+class Solution(object):
+    def canReach(self, arr, start):
+        """
+        :type arr: List[int]
+        :type start: int
+        :rtype: bool
+        """
+        q, lookup = collections.deque([start]), set([start])
+        while q:
+            i = q.popleft()
+            if not arr[i]:
+                return True
+            for j in [i-arr[i], i+arr[i]]:
+                if 0 <= j < len(arr) and j not in lookup:
+                    lookup.add(j)
+                    q.append(j) 
+        return False
+val=Solution()
+s,m=map(int,input().split())
+arr=list(map(int,input().split()))
+print(val.canReach(arr,m))
